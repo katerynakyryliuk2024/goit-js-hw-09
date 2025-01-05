e6424f8d005a20a001607e07d2b93b893e47f6a2
katerynakyryliuk2024 <katerynaerdek@gmail.com>
2025-01-05 14:43:37 +0000

    Deploying to gh-pages from @ katerynakyryliuk2024/goit-js-hw-09@16372a6ea9828784f686b1c4cba1408d74981279 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as o}from"./assets/vendor-B-WyX8ve.js";console.log(o);
+//# sourceMappingURL=1-gallery.js.map
